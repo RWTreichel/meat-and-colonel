161e0e1b7c5d9c0567bf37235c5eadf8cd7a26f9
Tyler Travers <ttravers17@gmail.com>
2015-06-18 18:05:56 -0400

    added routing

---
+++ client/app/app.js
@@ -0,0 +1,24 @@
+angular.module('Game', [
+
+])
+.config(function($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/grid/grid.html',
+      controller: 'gridCtrl'
+    });
+})
+// .run(function ($rootScope, $location, Auth) {
+//   // here inside the run phase of angular, our services and controllers
+//   // have just been registered and our app is ready
+//   // however, we want to make sure the user is authorized
+//   // we listen for when angular is trying to change routes
+//   // when it does change routes, we then look for the token in localstorage
+//   // and send that token to the server to see if it is a real user or hasn't expired
+//   // if it's not valid, we then redirect back to signin/signup
+//   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+//     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+//       $location.path('/signin');
+//     }
+//   });
+// });
