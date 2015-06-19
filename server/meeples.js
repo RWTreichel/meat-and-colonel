df40e25e5c0ca19497a95a996dcb48452469ac2b
Stephan Tabor <stephantabor@gmail.com>
2015-06-18 23:13:27 -0400

    refactor meeples

--- server/meeples.js
+++ server/meeples.js
@@ -1,28 +1,22 @@
-var Meeple = function(color, id){
+var Meeple = function(color, id, occupation, location){
   this.color = color;
   this.id = id;
-  this.occupation = null;
-  this.location = null; // int 0-8 based on placement on tile?
+  this.occupation = occupation;
+  this.location = location; // int 0-8 based on placement on tile?
 };
 
+// creates a collection of all of the meeples needed for one game
+// each player's meeples are in their own array
+// i.e. var meeps = new Meeples(); meeps.blue === [{meeple},{meeple} ...]
 var Meeples = function(){
-  this.blue   = [];
-  this.red    = [];
-  this.black  = [];
-  this.yellow = [];
-  this.green  = [];
-
-  for (var i = 0; i < 5; i++) {
-    this.blue.push( new Meeple('blue', i) );
-    this.red.push( new Meeple('red', i) );
-    this.black.push( new Meeple('black', i) );
-    this.yellow.push( new Meeple('yellow', i) );
-    this.green.push( new Meeple('green', i) );
+  var colors = ['blue', 'red', 'black', 'yellow', 'green'];
+  for (var j = 0; j < colors.length; j++) {
+    var color = colors[j];
+    this[color] = [];
+    for (var k = 0; k < 5; k++) {
+      this[color].push( new Meeple(color, k) );
+    }
   }
 };
 
 module.exports = Meeples;
-
-var meeps = new Meeples();
-
-console.log(meeps.blue);
