bb64b348632c87d814795597ad72bc2648ec43f5
Richard Treichel <rwtreichel@gmail.com>
2015-06-22 18:56:29 -0400

    Games now track current player, update game status, and advance to next player.

--- server/game.js
+++ server/game.js
@@ -3,18 +3,21 @@ var Deck = require('./deck');
 var Meeples = require('./meeples');
 var spec = require('./deckSpec');
 
-var Game = function(boardSize, deckSpeck){
-  this.state = 'pregame'; // pregame, ongoing, done 
+// players argument should be `players` from app.js
+var Game = function(boardSize, deckSpec, players){
+  this.ongoing = true; // indicates that game is or is not in session 
   this.boardSize = boardSize;
-  this.board = [];
+  this.board = []; // probably don't even need this anymore
     for( var i = 0; i < boardSize; i++ ) {
       this.board.push( new Array(boardSize) );
     }
-
-  this.deck = new Deck(deckSpeck);
+  this.deck = new Deck(deckSpec);
   this.meeples = new Meeples();
-  this.players = {};
+  this.players = Object.keys(players);
+  this.currentPlayer = 0;
 };
+
+// TODO: decide where to place start tile on the board;
 
 // arg should be a parsed tile from the client post request
 Game.prototype.placeTile = function(tile) {
@@ -22,9 +25,23 @@ Game.prototype.placeTile = function(tile) {
   this.board[tile.x][tile.y] = tile;
 };
 
-// Temporarily used to instantiate the game.
-// This should NOT be here long-term.
-var game = new Game (72, spec);
+Game.prototype.nextPlayer = function() {
+  if (this.currentPlayer === this.players.length - 1) {
+    this.currentPlayer = 0;
+  } else {
+    this.currentPlayer++;
+  }
+  return this.players[ this.currentPlayer ];
+};
 
-module.exports = game;
+// generates game state for next turn
+Game.prototype.update = function(tile) {
+  var gameState = {};
+  this.placeTile(tile);
+  gameState.lastTile = tile;
+  gameState.nextPlayer = this.nextPlayer();
+  gameState.nextTile = this.deck.pop();
+  return gameState;
+};
 
+module.exports = Game;
