652f65091e52aea47a0f6dade165eae28430cdbe
Tyler Travers <ttravers17@gmail.com>
2015-06-22 14:06:42 -0400

    setting background image property on cells

--- client/app/grid/grid.js
+++ client/app/grid/grid.js
@@ -1,21 +1,52 @@
 // Create our grid module namespace
 var grid = angular.module('game.grid', []);
 
+grid.factory('TileModel', function() {
+  // Returns a tile object
+  var Tile = function(pos, img, val) {
+    this.x = pos.x;
+    this.y = pos.y;
+    this.img = img;
+    this.val = val || 2;
+    this.orientation = 0;
+    this.meeples = [];
+  }
+  return Tile;
+});
+
+grid.service('GridService', function(TileModel) {
+  this.grid = [];
+  this.tiles = [];
+  this.size = 15;
+
+  this.buildEmptyGameBoard = function() {
+    var self = this;
+    for (var x = 0; x < self.size * self.size; x++) {
+      this.grid[x] = null;
+    }
+  };
+});
+
 // Create our grid controller
-grid.controller('gridCtrl', function($scope){
+grid.controller('gridCtrl', function($scope, TileModel){
   // A function that returns an array of length n
   $scope.range = function(n) {
     return new Array(n);
   };
   $scope.init = function() {
-    // Create our controller
   };
-  $scope.clickCell = function($event, x, y) {
-    // setCell(x, y);
-    console.log($event.target);
+  $scope.clickCell = function(event, x, y) {
+    var tile = new TileModel({x:x, y:y}, 'assets/img/Tiles/B.png');
+    setCell(event.target, tile);
   };
 
-  var setCell = function($event, x, y) {
+  var setCell = function(target, tile) {
+    console.log('tile is set');
+    console.log(tile);
+    angular.element(target).css('background-size', 'contain');
+    angular.element(target).css('background-image', 'url('+tile.img+')');
+
+    // $(event.target).css('display', 'none');
     // $scope.tile = {x: x, y: y};
     // console.log($event.target);
     // return angular.element($event.target);
