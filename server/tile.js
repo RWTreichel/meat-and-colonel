e592c9136ed0f06cdb930e7429e22d503b8bcf21
Stephan Tabor <stephantabor@gmail.com>
2015-06-22 22:05:57 -0400

    refactor Tile, Deck, and deckSpec to add land features to tiles

--- server/tile.js
+++ server/tile.js
@@ -1,19 +1,62 @@
 /* this is what a tile should look like going to and coming from client
     {
-      id: v9,          // (A thru X) + num
+      id: a,          // (A thru X) + num
       orientation: 1,  // 0 - 3
       x: 13,           // 0 - boardSize
       y: 2,            // 0 - boardSize
-      meeple: {}       // ref to actual meeple object see ./meeples.js
+      meeple: ** to do, meeple location stuff**
+      feature = {
+        n: 'grass',
+        s: 'road',
+        e: 'grass',
+        w: 'grass'
+      }
     } 
 */
 
-var Tile = function(id, x, y, meeple){
+var Tile = function(id, features, x, y, meeple){
   this.id = id;
-  this.orientation = 0;
   this.x = x;
   this.y = y;
+  this.orientation = 0;
+  this.features = features;
   this.meeple = meeple;
 };
 
+// use this on client side and remove from here.
+// will make tile validation easier if features
+// rotate with tile.
+Tile.prototype.rotateRight = function() {
+  this.orientation = (this.orientation + 1) % 4;
+
+  var temp = {};
+  for (var key in this.features){
+    temp[key] = this.features[key];
+  }
+
+  this.features.n = temp.w;
+  this.features.e = temp.n;
+  this.features.s = temp.e;
+  this.features.w = temp.s;
+};
+
 module.exports = Tile;
+
+// Below here is a simple demo, uncomment to see rotation in action
+// rotation should be a client side function, but it's here until
+// someone on front end needs it
+
+// var feat = {
+//       n: 'grass',
+//       s: 'road',
+//       e: 'grass',
+//       w: 'grass'
+//     };
+
+// var tile = new Tile('a', feat);
+
+// console.log(tile);
+// tile.rotateRight();
+// console.log(tile);
+// tile.rotateRight();
+// console.log(tile);
