5d20cf059abfcb889620cd52de6592034a35779b
Stephan Tabor <stephantabor@gmail.com>
2015-06-25 10:11:53 -0400

    make comments relevant to current code state reject logins for more than 5 users make sure 'numReady' is emitting when it needs to

--- app.js
+++ app.js
@@ -13,13 +13,8 @@ var spec = require('./server/deckSpec');
 var game, readyCount = 0;
 
 app.set('port', (process.env.PORT || 3000));
-
 app.use(express.static(__dirname + '/client'));
-
-// Attach session
 app.use(session);
-
-// Share session with io sockets
 io.use(sharedSession(session));
 
 // object holds player username, socket id, and password
@@ -31,47 +26,58 @@ io.on('connection', function(socket) {
     socket.handshake.session.userdata = userdata;
     socket.handshake.session.save();
 
+    // if player already exists in players obj we want to log them back in and not
+    // create a new user or overwrite their existing stuff other than socket id
+    // currently useless as the client does not support revival of the lost game state
     if (players[userdata.username]){
-      // check for password match
       if (userdata.password === players[userdata.username].password) {
-        // Move on with valid login stuff...
-        console.log("Welcome back!");
         players[userdata.username].socket = socket.id;
       } else {
-        // Invalid login handling...
+        // TODO: Invalid login handling...
         console.log('Wrong password!');
       }
     } else {
       // If player is logging in for the first time, register
       // their info on the players object
-      players[ userdata.username ] = { 
-        password: userdata.password,
-        color: userdata.color, 
-        numMeeps: 7, 
-        socket: socket.id,
-        ready: false
-      };
+      // only do this if the lobby isn't full
+      if (Object.keys(players).length < 5) {
+        players[ userdata.username ] = { 
+          password: userdata.password,
+          color: userdata.color, 
+          numMeeps: 7, 
+          socket: socket.id,
+          ready: false
+        };
+      } else {
+        // TODO: alert that the game is full
+        console.log('game full');
+      }
       console.log(players);
     }
+    // emit numready whenever there is a new connection
+    // used to display number of users connected and ready to play on the home page
     io.emit('numReady', {ready: readyCount, total: Object.keys(players).length});
   });
 
+  // currently unused on the client side
   socket.on('logout', function(userdata) {
     if (socket.handshake.session.userdata) {
       delete socket.handshake.session.userdata;
-      // Save the data to the session store
       socket.handshake.session.save();
-        io.emit('numReady', {ready: readyCount, total: Object.keys(players).length});
-
+      io.emit('numReady', {ready: readyCount, total: Object.keys(players).length});
     }
   });
 
-  socket.on('endTurn', function(data) { // data will be the tile they placed
+  // data will be the tile they placed
+  socket.on('endTurn', function(data) { 
    // verify that player ending turn, is actually the current player
+   // there might be a better way to do this using the session
     var name = game.players[ game.currentPlayer ];
 
-    if (players[name].socket === socket.id) {
-      var gameState = game.update(data); // return gamestate
+    if (players[ name ].socket === socket.id) {
+      // this updates the server side game object with all of the info from the
+      // player move. will be useful for implementing client reconnections
+      var gameState = game.update(data);
 
       // makes 'nextPlayer' the socket id; client needs to check this
       // to decide if it is their turn;
@@ -79,31 +85,29 @@ io.on('connection', function(socket) {
 
       // emit next turn to all connected sockets
       io.emit('nextTurn', gameState);
-
-    } else {
-      console.log("Not your turn!");
     }
+      // TODO: (maybe) alert the client that they are trying to end their turn
+      // when it isn't their turn
   });
 
-  // indicates that all parties are ready to play
-  // potentially refactor to need 'ready' from all connected players
+  // indicates that parties are ready to play
   socket.on('playerReady', function(data){
 
-    // on 'playersRead' emission the client needs to send their username;
+    // on 'playersReady' emission the client needs to send their username;
+    // maybe refactor to also require the socket.id so that user A can't ready 
+    // user B through the console
     var username = data;
 
-    // so if somehow the wrong username gets sent it won't crash the thing
-    if (players[username] === undefined || players[username].ready){
+    // prevent nonexistent username from doing stuff
+    // and it won't do stuff if the player is already 'ready'
+    if (players[ username ] === undefined || players[username].ready){
       return;
     }
 
-    // mark each player as ready as they emit this. 
-    // when all players are ready emit an 'all players ready'
-
     players[ username ].ready = true;
     readyCount++;
-
     var allReady = true;
+    
     for(var player in players){
       allReady = allReady && players[player].ready;
     }
@@ -118,22 +122,23 @@ io.on('connection', function(socket) {
         gameState.nextPlayer = players[ gameState.nextPlayer ].socket;
         io.emit('nextTurn', gameState);
       } else {
+        // TODO: alert that there are not enough people to play the game
+        //   also be too many if the untested code to block too many logins fails
         console.log('Invalid number of players: ', Object.keys(players).length);
       }
-    } else {
-      // if everyone isn't ready emit the num that are
-      io.emit('numReady', {ready: readyCount, total: Object.keys(players).length});
     }
+    // emit numReady every time someone readys up 
+    io.emit('numReady', {ready: readyCount, total: Object.keys(players).length});
   });
 
   // send num of meeps and meep color to client side
   socket.on('meepDataReq', function(data) {
     // client side sends current username
     if (data.numMeeps) {
-      players[data.username].numMeeps = data.numMeeps;
+      players[ data.username ].numMeeps = data.numMeeps;
       socket.emit('meepDataRes', { numMeeps: data.numMeeps });
     } else {
-      socket.emit('meepDataRes', { numMeeps: players[data.username].numMeeps });
+      socket.emit('meepDataRes', { numMeeps: players[ data.username ].numMeeps });
     }
     // var meepColor = players[data.username].color;
   });
