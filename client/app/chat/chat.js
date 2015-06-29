bc43860bcf7f1eaf74f98b09d9f0600f7991abce
Richard Treichel <rwtreichel@gmail.com>
2015-06-29 19:45:03 -0400

    Bugfix for multiple identical chat messages being sent.

--- client/app/chat/chat.js
+++ client/app/chat/chat.js
@@ -3,13 +3,18 @@ angular.module('game.chat', [])
     $scope.messages = [];
 
     socket.on('sendMessage', function(message) {
-      $scope.messages.push(message);
+      // Add the message to the front of the messages
+      // array so it displays messages in reverse chronological order.
+      $scope.messages.unshift(message);
       $scope.$apply();
     });
 
     $scope.sendMessage = function() {
+      // Cap a player's username to the first
+      // ten characters and escape the message input.
       var message = Player.getUsername().slice(0,10) + ': ' + _.escape($scope.message);
 
+      // Emit the message and clear the input box.
       socket.emit('sendMessage', message);
       $scope.message = '';
     };
